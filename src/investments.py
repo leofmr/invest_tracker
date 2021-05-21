7720ba3b08da870fd000a0e2bb6b9a4e9385dcb2
leofmr <leonardo.rocha@fiocruz.br>
2021-05-21 20:38:54 -0300

    Add investment class representation

--- src/investments.py
+++ src/investments.py
@@ -11,19 +11,30 @@ class Investments:
 
     
     def withdraw(self, value: float, invest_date=None):
-        self._insert_cash_flow_change(invest_date, value, False)
+        self._insert_cash_flow_change(invest_date=invest_date, value=value, is_positive=False)
         
 
     def invest(self, value: float, invest_date=None):
-        self._insert_cash_flow_change(invest_date, value, True)
+        self._insert_cash_flow_change(invest_date=invest_date, value=value, is_positive=True)
 
 
     def _insert_cash_flow_change(self, invest_date, value, is_positive):
+        reg = self._format_reg(reg_date=invest_date, value=value, is_positive=is_positive)
+        self.cash_flow = self.cash_flow.append(reg, ignore_index=True)
+
+    def update_value(self, value, measure_date=None):
+        reg = self._format_reg(reg_date=measure_date, value=value, is_positive=True)
+        self.current_values = self.current_values.append(reg, ignore_index=True)
+    
+    @staticmethod
+    def _format_reg(reg_date, value, is_positive):
         value = value if is_positive else -value
-        if invest_date is None:
+        if reg_date is None:
             date_obj = date.today()
         else: 
-            date_obj = datetime.strptime(invest_date, "%d/%m/%Y").date()
-        reg = pd.Series({"date": date_obj, "value": value})
-        
-        self.cash_flow = self.cash_flow.append(reg, ignore_index=True)
+            date_obj = datetime.strptime(reg_date, "%d/%m/%Y").date()
+        return pd.Series({"date": date_obj, "value": value})
+
+    def __repr__(self) -> str:
+        total_invest = self.cash_flow['value'].sum()
+        return f'Total investment: {total_invest}'
