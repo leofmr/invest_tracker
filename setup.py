73744a50cb339a5a9439d7232c8cc51dafcf0f0f
leofmr <leonardo.rocha@fiocruz.br>
2021-05-21 13:44:30 -0300

    Add initial configuration of the package
    The parameters in the setup were copied from an example.
    I don't know if they are correct and they are only temporary.

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name="invest_traker",
+      version="0.1",
+      description="Help manage investment registers",
+      packages=["invest_tracker"],
+      zip_safe=False)
