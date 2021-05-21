07a5a8b06096295321c4074b69dd9e42d6e11673
leofmr <leonardo.rocha@fiocruz.br>
2021-05-21 18:41:23 -0300

    Add cash_flow change methods

---
+++ src/example.py
@@ -0,0 +1,7 @@
+from investments import Investments
+
+asset_X = Investments()
+asset_X.invest(value=2000, invest_date="10/01/2005")
+asset_X.withdraw(value=100, invest_date="01/01/1999")
+asset_X.invest(value=1000)
+print(asset_X.cash_flow)
--- src/investments.py
+++ src/investments.py
@@ -1,13 +1,29 @@
 import pandas as pd
+from datetime import date, datetime
 
 class Investments:
+    """Primeira coisa que eu preciso identificar é a de quantidade. 
+    """    
+    
     def __init__(self):
-
-        self.assets_list = None
-        self.assets_regs = None
-
-    
-    def import_investments(self, filepath):
-        self.assets_regs = pd.read_csv(filepath=filepath)
+        self.cash_flow = pd.DataFrame(columns=["date", "value"])
+        self.current_values = pd.DataFrame(columns=["date", "value"])
 
     
+    def withdraw(self, value: float, invest_date=None):
+        self._insert_cash_flow_change(invest_date, value, False)
+        
+
+    def invest(self, value: float, invest_date=None):
+        self._insert_cash_flow_change(invest_date, value, True)
+
+
+    def _insert_cash_flow_change(self, invest_date, value, is_positive):
+        value = value if is_positive else -value
+        if invest_date is None:
+            date_obj = date.today()
+        else: 
+            date_obj = datetime.strptime(invest_date, "%d/%m/%Y").date()
+        reg = pd.Series({"date": date_obj, "value": value})
+        
+        self.cash_flow = self.cash_flow.append(reg, ignore_index=True)
