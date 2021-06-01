49a34943fe6df16d38f3166a3c15e932f4e720bb
leofmr <leonardo.rocha@fiocruz.br>
2021-06-01 10:24:28 -0300

    Fix setup name mispelling

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import pathlib
 here = pathlib.Path(__file__).parent.resolve()
 long_description = (here / 'README.md').read_text(encoding='utf-8')
 
-setup(name="invest_traker",
+setup(name="invest_tracker",
       version="0.1",
       description="Helps to track and manage investments",
       long_description=long_description,
